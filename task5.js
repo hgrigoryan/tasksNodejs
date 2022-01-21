ee4445c69beddfec9d388c8ed77b9cc821c483e2
Hasmik Grigoryan <hasmik.grigoryan9419@gmail.com>
2022-01-22 00:25:26 +0400

    Implementation: Reconstructed code, added new functions. TODO: Getted endless recursion.

--- task5.js
+++ task5.js
@@ -15,36 +15,78 @@ function Character(name){
     }
 }
 
-function generateCharacters(){
-    const heroes = [];
-    const villains = [];
+const heroes = [];
+const villains = [];
 
-    for(let i = 0; i < 10; ++i){
-        heroes[i] = new Character("hero" + (i+1));
-        villains[i] = new Character("villain" + (i+1));
+for(let i = 0; i < 10; ++i){
+    heroes[i] = new Character("hero" + (i+1));
+    villains[i] = new Character("villain" + (i+1));
+}
+
+function removeDeadCharacter(character){
+    index = heroes.indexOf(character);
+    if(index === -1){
+        index = villains.indexOf(character);
+        villains.splice(index,1);
     }
-
-    return [heroes, villains];
+    heroes.splice(index,1);
+    console.log(character.name + " dies");
 }
 
-function attack(hero, villain){
-    
+function attack(attacker, defencer){
+    let index;
+    let removed = false;
+    if(attacker.health <= 0){
+        removeDeadCharacter(attacker);
+        removed = true;
+    }
+    else if(defencer.health <= 0){
+        removeDeadCharacter(attacker);
+        removed = true;
+    }
+    if(!removed){
+        let attackInfo = attacker.name + "[" + attacker.health + "] hits " + defencer.name + "[" + defencer.health + "] with a power of " + attacker.power;
+        console.log(attackInfo);
+        defencer.health -= attacker.power;
+    }
 }
 
-function chooseRandomActor(arr){
-    let attackerName = arr[Math.floor(Math.random() * 9)];
-    console.log("attackerName: ", attackerName);
-    return attackerName;
+function chooseRandomCharacterAndAttac(attacker){
+    let defencer = arr[Math.floor(Math.random() * 9)];
+        attack(attacker, defencer);
+}
+
+function printWinnerInfo(winner){
+    let liveCharactersInfo = "[";
+    for(let i = 0; i < winner.length; ++i){
+        liveCharactersInfo = liveCharactersInfo + winner[i].name + "[" + winner[i].health + "] ";
+    }
+    liveCharactersInfo += "]";
+    if(winner === heroes){
+        console.log("Heroes win!");
+    }else{
+        console.log("Villains win!");
+    }
+    console.log(liveCharactersInfo);
 }
 
 function startBattle(){
-    [heroes, villains] = generateCharacters();
-    let whoStarts = (Math.random() * 2) < 1 ? "heroes" : "villains";
-    for(let i = 0; i < 50; ++i){
-        chooseRandomActor(heroes);
-        chooseRandomActor(villains);
+
+    if(heroes.length === 0){
+        printWinnerInfo(heroes);
+        return;
+    }
+    else if(villains.length === 0){
+        printWinnerInfo(villains);
+        return;
     }
     
+    const allCharacters = villains.concat(heroes);
+    for(let i = 0; i < allCharacters.length; ++i){
+       setTimeout(chooseRandomCharacterAndAttac, 1/allCharacters[i].speed*5, allCharacters[i]);
+   }
+   // Call start Battle recurcively till one of character arrayes will be empty
+   startBattle();
 }
 
 startBattle();
