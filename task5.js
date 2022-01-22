0d0b5b4f9a04e4453e4e26de5b9da7eac05b9410
Hasmik Grigoryan <hasmik.grigoryan9419@gmail.com>
2022-01-23 00:25:59 +0400

    Implementation: Made changes in several functions, works ok. TODO: Optimization needed.

--- task5.js
+++ task5.js
@@ -1,18 +1,9 @@
 function Character(name){
     this.name = name;
-    this.speed = (Math.random() * 5).toFixed(3);
-    // adding 1 if speed <= 4, to get 
-    // speed value between (0,5]
-    if(this.speed <= 4){
-        this.speed++;
-    }
+    this.speed = +((Math.random() * 4 + 1).toFixed(3));
+
     this.health = 100;
-    this.power = (Math.random() * 10).toFixed(3);
-    // adding 1 if speed <= 4, to get 
-    // speed value between (0,10]
-    if(this.power <= 9){
-        this.power++;
-    }
+    this.power = +((Math.random() * 9 + 1).toFixed(3)); 
 }
 
 const heroes = [];
@@ -26,42 +17,56 @@ for(let i = 0; i < 10; ++i){
 function removeDeadCharacter(character){
     index = heroes.indexOf(character);
     if(index === -1){
+
         index = villains.indexOf(character);
         villains.splice(index,1);
+    }else{
+        heroes.splice(index,1);
     }
-    heroes.splice(index,1);
     console.log(character.name + " dies");
 }
 
-function attack(attacker, defencer){
+function attack(attacker, defender){
     let index;
     let removed = false;
-    if(attacker.health <= 0){
+    if( attacker === undefined || attacker.health <= 0){
         removeDeadCharacter(attacker);
         removed = true;
     }
-    else if(defencer.health <= 0){
+    else if(defender === undefined || defender.health <= 0){
         removeDeadCharacter(attacker);
         removed = true;
     }
     if(!removed){
-        let attackInfo = attacker.name + "[" + attacker.health + "] hits " + defencer.name + "[" + defencer.health + "] with a power of " + attacker.power;
+        let attackInfo = attacker.name + "[" + attacker.health + "] hits " + defender.name + "[" + defender.health + "] with a power of " + attacker.power;
         console.log(attackInfo);
-        defencer.health -= attacker.power;
+        defender.health -= attacker.power;
     }
 }
 
-function chooseRandomCharacterAndAttac(attacker){
-    let defencer = arr[Math.floor(Math.random() * 9)];
-        attack(attacker, defencer);
+function chooseRandomCharacterAndAttac(attacker, whoAttacs){;
+ 
+    if(heroes.length === 0 || villains.length === 0){
+        return;
+    }
+    if(whoAttacs === "hero"){
+        let defenderIndex = [Math.floor(Math.random() * villains.length)];
+        defender = villains[defenderIndex];
+    }else if(whoAttacs === "villain"){
+        let defenderIndex = [Math.floor(Math.random() * heroes.length)];
+        defender = heroes[defenderIndex];
+    }
+    //console.log("chooseRandomCharacterAndAttac: " + attacker.name + " attac " + defender.name );
+    attack(attacker, defender);
 }
 
 function printWinnerInfo(winner){
     let liveCharactersInfo = "[";
     for(let i = 0; i < winner.length; ++i){
-        liveCharactersInfo = liveCharactersInfo + winner[i].name + "[" + winner[i].health + "] ";
+        liveCharactersInfo = liveCharactersInfo + winner[i].name + "[" + winner[i].health + "]";
     }
     liveCharactersInfo += "]";
+    console.log("*************************Buttle ended*************************");
     if(winner === heroes){
         console.log("Heroes win!");
     }else{
@@ -73,18 +78,26 @@ function printWinnerInfo(winner){
 function startBattle(){
 
     if(heroes.length === 0){
-        printWinnerInfo(heroes);
-        return;
-    }
-    else if(villains.length === 0){
         printWinnerInfo(villains);
         return;
     }
+    else if(villains.length === 0){
+        printWinnerInfo(heroes);
+        return;
+    }
     
-    const allCharacters = villains.concat(heroes);
-    for(let i = 0; i < allCharacters.length; ++i){
-       setTimeout(chooseRandomCharacterAndAttac, 1/allCharacters[i].speed*5, allCharacters[i]);
+    for(let i = 0; i < heroes.length; ++i){
+        chooseRandomCharacterAndAttac(heroes[i], "hero");
+        if(heroes[i]){
+            setTimeout(chooseRandomCharacterAndAttac, 1/heroes[i].speed*5000, heroes[i], "hero");
+        }
    }
+   for(let i = 0; i < villains.length; ++i){
+    chooseRandomCharacterAndAttac(villains[i], "villain");
+    if(villains[i]){
+        setTimeout(chooseRandomCharacterAndAttac, 1/villains[i].speed*5000, villains[i], "villain");
+    }
+}
    // Call start Battle recurcively till one of character arrayes will be empty
    startBattle();
 }
