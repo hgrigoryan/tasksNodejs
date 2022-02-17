ec3240f401994a978ce8b11d6f2ac1f60c4ab2eb
Hasmik Grigoryan <hasmik.grigoryan9419@gmail.com>
2022-02-18 00:57:51 +0400

    Implementation: Added line formating before writing in file.

--- task7.js
+++ task7.js
@@ -17,14 +17,30 @@ function getFiles(dir){
     return fileInfo;
 }
 
+function formatInfoAndWriteToFile(fileInfoArr){
+    let maxLengthOfPath = 0;
+    let i;
+    for(i = 0; i < fileInfoArr.length - 1; ++i){
+        if(fileInfoArr[i+1][0].length > maxLengthOfPath){
+            maxLengthOfPath = fileInfoArr[i+1][0].length;
+        }
+    }
+    const maxLengthOfLine = maxLengthOfPath + 6;
+
+    let numberOfDashes;
+    let lengthOfPath;
+    const text = fs.createWriteStream("sorted_files.txt");
+    for(i = 0; i < fileInfoArr.length; ++i){
+        lengthOfPath = fileInfoArr[i][0].length + (fileInfoArr[i][1].toString()).length;
+        numberOfDashes = maxLengthOfLine - lengthOfPath;
+        text.write(fileInfoArray[i][0] + "-".repeat(numberOfDashes) + fileInfoArray[i][1] + "bytes\n");
+    }
+}
+
 const dirPath = process.argv[2];
 const fileInfoObj = getFiles(dirPath);
 const fileInfoArray = Object.entries(fileInfoObj);
 
 fileInfoArray.sort((a, b) => a[1] - b[1]);
 
-let i;
-const text = fs.createWriteStream("sorted_files.txt");
-for(i = 0; i < fileInfoArray.length; ++i){
-    text.write(fileInfoArray[i][0] + "--------" + fileInfoArray[i][1] + "bytes\n"); 
-}
+formatInfoAndWriteToFile(fileInfoArray);
