196e23dcca8f7eef586522000c1d15219da14ab8
Hasmik Grigoryan <hasmik.grigoryan9419@gmail.com>
2022-02-26 23:24:18 +0400

    Initialization: Added functionality.

---
+++ task8/initialData.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {name: "Anna", phoneNumber: "+374 91000000"}, 
+    {name: "Ani", phoneNumber: "+374 91111111"},
+    {name: "Mane", phoneNumber: "+374 91222222"},
+    {name: "Karine", phoneNumber: "+374 98888888"},
+    {name: "Syune", phoneNumber: "+374 10222222"},
+    {name: "Serine", phoneNumber: "+374 96777777"},
+    {name: "Marine", phoneNumber: "+374 94555555"},
+    {name: "Nare", phoneNumber: "+374 93666666"},
+    {name: "Narine", phoneNumber: "+374 10333333"},
+]
--- task8/server.js
+++ task8/server.js
@@ -1,24 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const contacts = require("./initialData");
 
 const app = express();
 
 app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const tasks= [{name: "John", phoneNumber: "+374 10636363"}, 
-              {name: "Doe", phoneNumber: "+374 99636363"}];
 
 app.get("/", (req, res) => {
-    res.render("index", {tasks});
+    res.render("index", {contacts});
 })
 
 app.get("/contacts/new", (req, res) => {
     res.render("addContact", {});
 })
 
-app.get("/contacts/edit", (req, res) => {
-    res.render("editContact", {name: "John", phoneNumber: "+374 10636363" });
+app.post("/contacts/new", (req, res) => {
+    console.log(req.body);
+    contacts.push(req.body);
+    res.status(200).redirect("http://localhost:3000/").render("index", {contacts});
 })
 
+app.get("/contacts/edit", (req, res) => {
+    console.log(req.body);
+    res.render("editContact", {name: "John", phoneNumber: "+374 10636363" });
+})
 
 
 const PORT = process.env.PORT || 3000;
